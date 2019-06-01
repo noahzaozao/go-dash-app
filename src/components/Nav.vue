2ec16698bf45c73d4add99b8b0e7c6c550355686
wzy(Noah Wang) <234082230@qq.com>
2019-06-01 16:05:04 +0800

    update

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,28 +8,25 @@
         router>
         <el-menu-item index="dashboard">Dashboard</el-menu-item>
         <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="2-4-1">选项1</el-menu-item>
-                <el-menu-item index="2-4-2">选项2</el-menu-item>
-                <el-menu-item index="2-4-3">选项3</el-menu-item>
+            <template slot="title">Control Console</template>
+            <el-menu-item index="">Option 1</el-menu-item>
+            <el-menu-item index="">Option 2</el-menu-item>
+            <el-submenu index="">
+                <template slot="title">Option 3</template>
+                <el-menu-item index="">Option 1</el-menu-item>
+                <el-menu-item index="">Option 2</el-menu-item>
             </el-submenu>
         </el-submenu>
-        <el-menu-item index="3" disabled>消息中心</el-menu-item>
-        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+        <el-menu-item index="">Notification Center</el-menu-item>
+        <el-menu-item index="">Management</el-menu-item>
         <el-dropdown>
             <i class="el-icon-setting" style="margin-right: 15px; color: #fff"></i>
             <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>查看</el-dropdown-item>
-                <el-dropdown-item>新增</el-dropdown-item>
-                <el-dropdown-item>删除</el-dropdown-item>
+                <el-dropdown-item>Settings</el-dropdown-item>
+                <el-dropdown-item>Logout</el-dropdown-item>
             </el-dropdown-menu>
         </el-dropdown>
-        <span style="font-size: 12px; color: #fff; margin-right: 15px;">王小虎</span>
+        <span style="font-size: 12px; color: #fff; margin-right: 15px;">Username</span>
     </el-menu>
 </template>
 
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,12 +1,15 @@
 <template>
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-        <el-menu :default-openeds="['1']" router>
+        <el-menu
+                :default-active="$route.path"
+                :default-openeds="['1']"
+                router>
             <el-submenu index="1">
                 <template slot="title"><i class="el-icon-message"></i>Microservices</template>
                 <el-menu-item-group>
                     <template slot="title">ETCD</template>
-                    <el-menu-item index="/"><i class="el-icon-star-on"></i>Home</el-menu-item>
-                    <el-menu-item index="/about"><i class="el-icon-star-on"></i>About</el-menu-item>
+                    <el-menu-item index="/">Home</el-menu-item>
+                    <el-menu-item index="/about">About</el-menu-item>
                 </el-menu-item-group>
             </el-submenu>
         </el-menu>
